c7b6d8413c2d5656d3634687c73436fe29aca9b0
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2024-05-15 11:56:00 +0000

    sono arrivato al ping del server

--- server/app.py
+++ server/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def ping_server():
-    return "Welcome to the world of animals."
+    return "oksdioahdi"
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
